13f4e3848a27be378f196b32ee74dd0f0e147196
ECR\Egon Rasmussen <ecr@live.dk>
2019-03-07 10:06:20 +0100

    Add project files.

---
+++ Modeling/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Modeling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AppContext context = new AppContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                System.Console.WriteLine("Database deleted and created!");
+
+                var blog = new Blog
+                {
+                    Url = "https://itdata.net/blog/1",
+                    BlogImage = new BlogImage
+                        {
+                            Caption = "Image Blog 1"
+                        }
+                };
+                context.Add<Blog>(blog);
+                context.SaveChanges();
+
+                Blog firstBlog = context.Blogs.Include(b => b.BlogImage).FirstOrDefault();
+
+                System.Console.WriteLine($"Blog URL: {firstBlog.Url} - Image caption: {firstBlog.BlogImage.Caption}");
+            }
+        }
+    }
+}
