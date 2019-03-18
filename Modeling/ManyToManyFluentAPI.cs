c88a03796e8322e0a381c2c9cd0d3547b84fce4f
Egon C. Rasmussen <ecr@eucsyd.dk>
2019-03-18 12:35:27 +0100

    ok

--- Modeling/ManyToManyFluentAPI.cs
+++ Modeling/ManyToManyFluentAPI.cs
@@ -14,6 +14,16 @@ namespace Modeling
         {
             modelBuilder.Entity<PostTag>()
                 .HasKey(t => new { t.PostId, t.TagId });
+
+            modelBuilder.Entity<PostTag>()
+            .HasOne(pt => pt.Post)
+            .WithMany(p => p.PostTags)
+            .HasForeignKey(pt => pt.PostId);
+
+            modelBuilder.Entity<PostTag>()
+                .HasOne(pt => pt.Tag)
+                .WithMany(t => t.PostTags)
+                .HasForeignKey(pt => pt.TagId);
         }
 
         public DbSet<Post> Posts { get; set; }
