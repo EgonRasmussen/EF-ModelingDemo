0f4df9066a447bb1fc5b582a1a81f151fc641890
Egon C. Rasmussen <ecr@eucsyd.dk>
2019-03-18 12:33:41 +0100

    ok

---
+++ Modeling/ManyToManyFluentAPI.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Modeling
+{
+    class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = BloggingDb; Trusted_Connection = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PostTag>()
+                .HasKey(t => new { t.PostId, t.TagId });
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public List<PostTag> PostTags { get; set; }
+    }
+
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+        public string TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+
+    public class Tag
+    {
+        public string TagId { get; set; }
+
+        public List<PostTag> PostTags { get; set; }
+    }
+
+    
+}
--- Modeling/Program.cs
+++ Modeling/Program.cs
@@ -12,21 +12,6 @@ namespace Modeling
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
                 System.Console.WriteLine("Database deleted and created!");
-
-                var blog = new Blog
-                {
-                    Url = "https://itdata.net/blog/1",
-                    BlogImage = new BlogImage
-                        {
-                            Caption = "Image Blog 1"
-                        }
-                };
-                context.Add<Blog>(blog);
-                context.SaveChanges();
-
-                Blog firstBlog = context.Blogs.Include(b => b.BlogImage).FirstOrDefault();
-
-                System.Console.WriteLine($"Blog URL: {firstBlog.Url} - Image caption: {firstBlog.BlogImage.Caption}");
             }
         }
     }
