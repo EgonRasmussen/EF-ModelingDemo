a80cfbcb5e865857b6671e9c2bf4adbd144a40b6
Egon C. Rasmussen <ecr@eucsyd.dk>
2019-03-18 11:02:38 +0100

    ok

--- Modeling/Program.cs
+++ Modeling/Program.cs
@@ -7,26 +7,11 @@ namespace Modeling
     {
         static void Main(string[] args)
         {
-            using (AppContext context = new AppContext())
+            using (BloggingContext context = new BloggingContext())
             {
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
                 System.Console.WriteLine("Database deleted and created!");
-
-                var blog = new Blog
-                {
-                    Url = "https://itdata.net/blog/1",
-                    BlogImage = new BlogImage
-                        {
-                            Caption = "Image Blog 1"
-                        }
-                };
-                context.Add<Blog>(blog);
-                context.SaveChanges();
-
-                Blog firstBlog = context.Blogs.Include(b => b.BlogImage).FirstOrDefault();
-
-                System.Console.WriteLine($"Blog URL: {firstBlog.Url} - Image caption: {firstBlog.BlogImage.Caption}");
             }
         }
     }
