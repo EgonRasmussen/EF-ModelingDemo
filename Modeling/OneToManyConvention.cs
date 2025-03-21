23f8c3ba5515f78b840e98446c877414f799ac6b
EgonRasmussen <ecr@eucsyd.dk>
2025-03-21 14:58:38 +0100

    .NET9

--- Modeling/OneToManyConvention.cs
+++ Modeling/OneToManyConvention.cs
@@ -2,33 +2,32 @@
 using System;
 using System.Collections.Generic;
 
-namespace Modeling
+namespace Modeling;
+
+class AppContext : DbContext
 {
-    class AppContext : DbContext
-    {
-        public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = BloggingDb; Trusted_Connection = True; ");
-        }
-    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+        .UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = BloggingDb; Trusted_Connection = True; ")
+        .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+}
 
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-        public ICollection<Post> Posts { get; set; }
-    }
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
 
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
 
-        public int BlogId { get; set; }     // FK
-        public Blog Blog { get; set; }
-    }
+    public int BlogId { get; set; }     // FK
+    public Blog Blog { get; set; }
 }
---
+++ Modeling/README.md
@@ -0,0 +1,9 @@
+# OneToMany Convention
+
+[Docs: Relationships](https://docs.microsoft.com/en-us/ef/core/modeling/relationships)
+
+[Entity Framework Tutorial - One-to-Many](https://www.entityframeworktutorial.net/efcore/one-to-many-conventions-entity-framework-core.aspx)
+
+
+![ER-diagram](BlogPostDiagram.png)
+
